78894a61377d252b8b286564b04eb727059ff141
NirChitlaru <nir.chitlaru@gmail.com>
2018-04-27 16:07:53 +0300

    27.4.18 16:05

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,15 @@
 package com.londonappbrewery.magiceightball;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button changeBallBtn ;
+        changeBallBtn = (Button)findViewById(R.id.changeBallBtn) ;
+        final ImageView ballImage ;
+        ballImage  = findViewById(R.id.ballImage) ;
+        ballImage.setTag(R.drawable.p1) ;
+        final ArrayList<Integer> ballStatePics = new ArrayList<Integer>();
+        ballStatePics.add(R.drawable.p1);
+        ballStatePics.add(R.drawable.p2);
+        ballStatePics.add(R.drawable.p3);
+
+
+
+        changeBallBtn.setOnClickListener(new View.OnClickListener()
+                                         {
+            @Override
+            public void onClick(View v) {
+
+                int num = ballStatePics.indexOf(ballImage.getTag()) + 1 ;
+                if (num > 2) num = 0 ;
+
+                ballImage.setImageResource(ballStatePics.get(num));
+                ballImage.setTag(ballStatePics.get(num));
+
+                Log.d("magicball" , "The image is"+ num ) ;
+
+            }
+        }
+        );
+
     }
 }
